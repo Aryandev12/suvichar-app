612b6a687c91f1e9e4e35c827e0f886d16ef2c0d
Aryandev12 <aryandev512@gmail.com>
2025-12-05 16:02:04 +0530

    counter.

--- App.tsx
+++ App.tsx
@@ -6,11 +6,13 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -25,13 +27,26 @@ function App() {
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
+  const [count,setCount] = useState(0);
+
+  const handlePress = () =>{
+    console.log(count);
+    const newcount = count+1;
+    setCount(newcount)
+  }
+
 
   return (
+    
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text>
+        {count}
+
+
+      </Text>
+      <Button title='click me' onPress={()=>{handlePress()}} />
+      
+    
     </View>
   );
 }
